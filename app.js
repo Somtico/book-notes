5dbed8fb647c44de24e33508fe5841dd3ce66a4e
Somtochukwu Ufondu <65261673+Somtico@users.noreply.github.com>
2024-12-10 11:55:20 -0600

    Make more secure

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
+import pool from "./db.js";
 
 const app = express();
 const port = 3000;
-const db = new pg.Client({
-  user: "somto",
-  host: "localhost",
-  database: "book_tracker",
-  port: 5435,
-  password: "1475",
-});
 
-db.connect();
+pool.connect();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.listen(port, () => {
